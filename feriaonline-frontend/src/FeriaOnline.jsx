d1c2212f5f831f9cde614bd092437a9e3f6ab2cf
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-03-28 18:25:06 -0300

    Creacion de productos Context/Provider y localización en  FelriaOnline.jsx

--- feriaonline-frontend/src/FeriaOnline.jsx
+++ feriaonline-frontend/src/FeriaOnline.jsx
@@ -1,25 +1,27 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { NavBar } from "./components/Generales/NavBar"
+import { ProductosProvider } from "./context/ProductosProvider"
+import { LoginPage } from "./pages/loginRegistroPages/LoginPage"
+import { RegistroPage } from "./pages/loginRegistroPages/RegistroPage"
 import { PerfilPage } from "./pages/PerfilPage"
 import { DescripcionPage } from "./pages/PerfilPages/DescripcionPage"
+import { EditarPerfil } from "./pages/PerfilPages/EditarPerfil"
 import { MisComprasPage } from "./pages/PerfilPages/MisComprasPage"
 import { MisPublicacionesPage } from "./pages/PerfilPages/MisPublicacionesPage"
 import { MisVentasPage } from "./pages/PerfilPages/MisVentasPage"
 import { ProductosPage } from "./pages/ProductosPage"
-import { VenderPage } from "./pages/VenderPage"
-import { LoginPage } from "./pages/loginRegistroPages/LoginPage"
-import { RegistroPage } from "./pages/loginRegistroPages/RegistroPage"
-import { EditarPerfil } from "./pages/PerfilPages/EditarPerfil"
 import { VerMasPage } from "./pages/productosPages/verMasPage"
+import { VenderPage } from "./pages/VenderPage"
+
 
 export const FeriaOnline = () => {
   return (
-    <>
+    <ProductosProvider>
       <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<ProductosPage />} />
         <Route path="/productos" element={<ProductosPage />} />
-        <Route path="/productos/descripcionProducto" element={<VerMasPage />} /> 
+        <Route path="/productos/descripcionProducto" element={<VerMasPage />} />
         <Route path="/vender" element={<VenderPage />} />
         <Route path="/perfil" element={<PerfilPage />}>
           <Route path="descripcion" element={<DescripcionPage />} />
@@ -33,7 +35,7 @@ export const FeriaOnline = () => {
         <Route path="/registro" element={<RegistroPage />} />
         <Route path="/*" element={<Navigate to='/' />} />
       </Routes>
+    </ProductosProvider>
 
-    </>
   )
 }
--- feriaonline-frontend/src/pages/ProductosPage.jsx
+++ feriaonline-frontend/src/pages/ProductosPage.jsx
@@ -1,15 +1,16 @@
+import { Outlet } from "react-router-dom";
 import { Card } from "../components/Inicio/Card";
 import { Ordenar } from "../components/inicio/Ordenar";
-import { Outlet } from "react-router-dom";
 
 export const ProductosPage = () => {
+
+
     return (
         <div className="container mt-4 text-center">
             <h1>Productos</h1>
             <hr />
             <Ordenar />
             <div className="row d-flex justify-content-center">
-                <Card />
                 <Card />
                 <Card />
                 <Card />
