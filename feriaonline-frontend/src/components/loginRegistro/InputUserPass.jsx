b136e0a01bc0cd1860105188860966dbfd9b033b
FacundoOrona <fcndorona@gmail.com>
2025-04-01 15:08:55 -0300

    Arreglos visuales
    Modificacion visual de los botones y estructura del login

--- feriaonline-frontend/src/components/loginRegistro/InputUserPass.jsx
+++ feriaonline-frontend/src/components/loginRegistro/InputUserPass.jsx
@@ -5,16 +5,18 @@ export const InputUserPass = () => {
         <div className="container mt-4 text-center">
             <form>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Usuario</label>
+                    <label htmlFor="exampleInputEmail1" className="form-label"><b>Usuario</b></label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+                    <label htmlFor="exampleInputPassword1" className="form-label"><b>Contraseña</b></label>
                     <input type="password" className="form-control" id="exampleInputPassword1" />
                 </div>
                 <button type="submit" className="btn btn-primary">Ingresar</button>
             </form>
-            <NavLink to='/registro' className="btn btn-secondary" href="#" role="button">Registrarse</NavLink>
+            <br />
+            <h6>No tienes cuenta? Presiona aqui para registrate.</h6>
+            <NavLink to='/registro' className="btn btn-link" href="#" role="button">Registrarse</NavLink>
         </div>
     )
 }
