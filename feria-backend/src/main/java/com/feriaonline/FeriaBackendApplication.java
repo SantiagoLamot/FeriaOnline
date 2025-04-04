b3c2a96e9a8ed935eb0933ce8f94346df6be2c24
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-04-04 16:13:31 -0300

    Creacion de proyecto Backend

---
+++ feria-backend/src/main/java/com/feriaonline/FeriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.feriaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriaBackendApplication.class, args);
+	}
+
+}
---
+++ feria-backend/src/test/java/com/feriaonline/FeriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feriaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
