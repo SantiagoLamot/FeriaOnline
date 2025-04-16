1a18255bed4840c016b8bd88c12abf50304efb4b
FacundoOrona <fcndorona@gmail.com>
2025-04-16 13:29:43 -0300

    Entidad publicaciones
    Created entidad de publicaciones y enum de estadoPublicaciones.
    Utilizando Lombok y Jakarta.

---
+++ feriaonline-backend/src/main/java/com/feriaonline/entidades/EstadoPublicacion.java
@@ -0,0 +1,6 @@
+package com.feriaonline.entidades;
+
+public enum EstadoPublicacion {
+    Activo,
+    Inactivo
+}
---
+++ feriaonline-backend/src/main/java/com/feriaonline/entidades/publicaciones.java
@@ -0,0 +1,42 @@
+package com.feriaonline.entidades;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Publicaciones_tb")
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class publicaciones {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_Publicacion")
+    private Integer id;
+
+    @Column(name = "NombreProducto", nullable = false, length = 100)
+    private String nombreProducto;
+
+    @Column(name = "Descripcion", columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(name = "Precio", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precio;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Estado", nullable = false)
+    private EstadoPublicacion estado;
+
+    //Descomentar cuando se haga entidad usuario
+    /*@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_UsuarioVendedor", nullable = false)
+    private Usuario usuarioVendedor;*/  
+    
+}
+
+
+
