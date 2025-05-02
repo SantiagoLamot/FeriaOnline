0e5fb019809ba71443f72a85ff4f312b3bcc3daf
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-05-02 12:29:53 -0300

    Correcciones varias
    Se actualizaron nombres de tablas y columnas en base de datos por snake_case por convencion de JPA y evitar errores, se quito la propiedad en application properties que permitia que cree nuevas columnas

--- feriaonline-backend/src/main/java/com/feriaonline/controller/PublicacionController.java
+++ feriaonline-backend/src/main/java/com/feriaonline/controller/PublicacionController.java
@@ -3,18 +3,17 @@ package com.feriaonline.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.feriaonline.entidadesDTO.PublicacionDTO;
 import com.feriaonline.service.PublicacionService;
 
 
-@Controller
-@RequestMapping("/api/hospedajes")
+@RestController
+@RequestMapping("/publicaciones")
 @CrossOrigin(origins = "*") // permitir requests del frontend
 public class PublicacionController {
     
@@ -22,7 +21,7 @@ public class PublicacionController {
     PublicacionService publicacionService;
 
     @GetMapping()
-    public List<PublicacionDTO> getMethodName(@RequestParam String param) {
+    public List<PublicacionDTO> ObtenerTodasLasPublicaciones() {
         return publicacionService.obtenerTodasLasPublicaciones();
     }
     
--- feriaonline-backend/src/main/java/com/feriaonline/entidades/ImagenPublicacion.java
+++ feriaonline-backend/src/main/java/com/feriaonline/entidades/ImagenPublicacion.java
@@ -15,7 +15,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "ImagenesxPublicacion_tb")
+@Table(name = "imagenes_por_publicacion_tb")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
