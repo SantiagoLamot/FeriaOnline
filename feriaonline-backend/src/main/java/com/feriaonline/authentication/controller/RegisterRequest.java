5b21e443d2d2da5d4b21c5b2098b0a43a29a90d4
FacundoOrona <fcndorona@gmail.com>
2025-05-23 12:28:24 -0300

    Create RegisterRequest.java

---
+++ feriaonline-backend/src/main/java/com/feriaonline/authentication/controller/RegisterRequest.java
@@ -0,0 +1,10 @@
+package com.feriaonline.authentication.controller;
+
+public record RegisterRequest(
+    String nombreDeUsuario,
+    String correo,
+    String contrasena,
+    String nombre,
+    String apellido
+) {
+}
