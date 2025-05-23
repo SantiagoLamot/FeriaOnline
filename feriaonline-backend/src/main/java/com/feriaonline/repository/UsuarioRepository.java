77fc011a1c900bfd812d2168765da5f3ef54da03
FacundoOrona <fcndorona@gmail.com>
2025-05-23 12:34:32 -0300

    Update UsuarioRepository.java

--- feriaonline-backend/src/main/java/com/feriaonline/repository/UsuarioRepository.java
+++ feriaonline-backend/src/main/java/com/feriaonline/repository/UsuarioRepository.java
@@ -1,9 +1,14 @@
 package com.feriaonline.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feriaonline.entidades.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer > {
     
+    Optional<Usuario>findByNombreDeUsuario(String nombreDeUsuario);
+
+    Optional<Usuario>findByCorreo(String correo);
 }
