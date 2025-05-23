7842ad3c63d17ea6c2b44b3b949cf9ba3f1780dc
FacundoOrona <fcndorona@gmail.com>
2025-05-23 12:50:40 -0300

    Register controller

---
+++ feriaonline-backend/src/main/java/com/feriaonline/authentication/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.feriaonline.authentication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.feriaonline.entidades.Usuario;
+import com.feriaonline.service.UsuarioService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registrar(@RequestBody RegisterRequest request) {
+        try {
+            Usuario usuario = usuarioService.registrarUsuario(request);
+            return ResponseEntity.ok(usuario);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
