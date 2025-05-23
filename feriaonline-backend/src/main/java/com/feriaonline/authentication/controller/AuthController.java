34efd4552855079223f270ecff26365a8c0da432
FacundoOrona <fcndorona@gmail.com>
2025-05-23 15:50:52 -0300

    controller register con jwtService

--- feriaonline-backend/src/main/java/com/feriaonline/authentication/controller/AuthController.java
+++ feriaonline-backend/src/main/java/com/feriaonline/authentication/controller/AuthController.java
@@ -1,13 +1,11 @@
 package com.feriaonline.authentication.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.feriaonline.entidades.Usuario;
-import com.feriaonline.service.UsuarioService;
+import com.feriaonline.authentication.service.AuthService;
 import org.springframework.web.bind.annotation.RequestBody;
 import lombok.RequiredArgsConstructor;
 
@@ -15,15 +13,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    
-    @Autowired
-    private UsuarioService usuarioService;
+
+    private final AuthService service;
 
     @PostMapping("/register")
     public ResponseEntity<?> registrar(@RequestBody RegisterRequest request) {
         try {
-            Usuario usuario = usuarioService.registrarUsuario(request);
-            return ResponseEntity.ok(usuario);
+            final TokenResponse response = service.registrarUsuario(request);
+            return ResponseEntity.ok(response);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
