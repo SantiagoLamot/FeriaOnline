e7ba3127feafaef9b4b8adf31a67f50147f00529
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-05-26 21:12:36 -0300

    Update AuthService.java

--- feriaonline-backend/src/main/java/com/feriaonline/authentication/service/AuthService.java
+++ feriaonline-backend/src/main/java/com/feriaonline/authentication/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.feriaonline.authentication.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.feriaonline.authentication.controller.LoginRequest;
 import com.feriaonline.authentication.controller.RegisterRequest;
 import com.feriaonline.authentication.controller.TokenResponse;
 import com.feriaonline.authentication.repository.Token;
@@ -15,7 +19,8 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-
+    @Autowired
+    private AuthenticationManager authenticationManager;
     private final UsuarioRepository usuarioRepository;
     private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
@@ -47,6 +52,15 @@ public class AuthService {
         saveUserToken(usuarioGuardado, jwtToken);
 
         return new TokenResponse(jwtToken, refreshToken);
+    }
+
+
+    public TokenResponse login(LoginRequest request){
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        Usuario usuario = usuarioRepository.findByNombreDeUsuario(request.getUsername()).orElseThrow();
+        String token = jwtService.generateToken(usuario);
+        String tokenR = jwtService.generateRefreshToken(usuario);
+        return new TokenResponse(token, tokenR);
     }
 
     private void saveUserToken(Usuario user, String jwtToken) {
