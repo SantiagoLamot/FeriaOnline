1e85804d84ab6bbbed81e6a09a3afdf6d9f981ef
FacundoOrona <fcndorona@gmail.com>
2025-06-03 14:29:04 -0300

    Funcionalidad realizarCompra

--- feriaonline-backend/src/main/java/com/feriaonline/service/VentasService.java
+++ feriaonline-backend/src/main/java/com/feriaonline/service/VentasService.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.feriaonline.dto.TransaccionRequest;
+import com.feriaonline.entidades.EstadoTransaccion;
+import com.feriaonline.entidades.Publicacion;
+import com.feriaonline.entidades.Transaccion;
+import com.feriaonline.entidades.Usuario;
 import com.feriaonline.repository.PublicacionRepository;
 import com.feriaonline.repository.TransaccionRepository;
 import com.feriaonline.repository.UsuarioRepository;
@@ -21,14 +25,27 @@ public class VentasService {
     private PublicacionRepository publicacionRepository;
 
     public void realizarCompra(TransaccionRequest compraDTO) {
-        // Aquí iría la lógica para procesar la compra
-        // Por ejemplo, verificar el usuario, la publicación y crear una transacción
-        // transaccionRepository.save(transaccion);
-        // usuarioRepository.updateUsuario(usuario);
-        // publicacionRepository.updatePublicacion(publicacion);
-        
-        // Lanzar una excepción si algo falla
-        // throw new RuntimeException("Error al procesar la compra");
+        //Buscar entidades relacionadas a la transaccion
+        Publicacion publicacion = publicacionRepository.findById(compraDTO.publicacionId())
+                .orElseThrow(() -> new RuntimeException("Publicación no encontrada"));
+
+        Usuario comprador = usuarioRepository.findById(compraDTO.compradorId())
+                .orElseThrow(() -> new RuntimeException("Comprador no encontrado"));
+
+        Usuario vendedor = usuarioRepository.findById(compraDTO.vendedorId())
+                .orElseThrow(() -> new RuntimeException("Vendedor no encontrado"));
+
+        EstadoTransaccion estado = EstadoTransaccion.valueOf(compraDTO.estado());
+
+        Transaccion transaccion = Transaccion.builder() // HAY QUE AGREGAR MONTO
+                .publicacion(publicacion)
+                .comprador(comprador)
+                .vendedor(vendedor)
+                .metodoDePago(compraDTO.metodoDePago())
+                .estado(estado) 
+                .build();
+
+        transaccionRepository.save(transaccion);
     }
     
 }
