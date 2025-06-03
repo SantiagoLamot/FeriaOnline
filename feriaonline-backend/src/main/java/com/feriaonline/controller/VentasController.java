a3737b8e2f1edf09a828f7d2931533b8d9fb8198
FacundoOrona <fcndorona@gmail.com>
2025-06-03 14:14:24 -0300

    Create VentasController.java

---
+++ feriaonline-backend/src/main/java/com/feriaonline/controller/VentasController.java
@@ -0,0 +1,30 @@
+package com.feriaonline.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import com.feriaonline.dto.TransaccionRequest;
+import com.feriaonline.service.VentasService;
+
+@RestController
+@RequestMapping("/ventas")
+public class VentasController {
+    
+    @Autowired
+    private VentasService ventasService;
+
+    @PostMapping("/compra")
+    public ResponseEntity<String> realizarCompra(@RequestBody TransaccionRequest compraDTO) {
+        try {
+            ventasService.realizarCompra(compraDTO);
+            return ResponseEntity.ok("Compra realizada con éxito");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al realizar la compra: " + e.getMessage());
+        }
+    }
+    
+}
