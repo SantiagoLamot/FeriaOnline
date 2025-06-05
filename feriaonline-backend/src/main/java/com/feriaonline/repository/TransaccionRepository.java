d4274b91be182c55c809ecd447ac1f0d2706ba20
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-05 00:07:30 -0300

    Transacciones Repository

--- feriaonline-backend/src/main/java/com/feriaonline/repository/TransaccionRepository.java
+++ feriaonline-backend/src/main/java/com/feriaonline/repository/TransaccionRepository.java
@@ -1,10 +1,14 @@
 package com.feriaonline.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.feriaonline.entidades.Transaccion;
 
 @Repository
 public interface TransaccionRepository extends JpaRepository<Transaccion, Integer> {
+    List<Transaccion> findByUsuarioVendedor_Id(int id);
 }
 
