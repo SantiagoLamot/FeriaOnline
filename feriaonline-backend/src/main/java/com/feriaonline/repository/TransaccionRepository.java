9d44e3dff0c75f66ff5676a55f1271e66b316312
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-06 15:53:52 -0300

    Mis ventas desde usuario logueado

--- feriaonline-backend/src/main/java/com/feriaonline/repository/TransaccionRepository.java
+++ feriaonline-backend/src/main/java/com/feriaonline/repository/TransaccionRepository.java
@@ -10,5 +10,7 @@ import com.feriaonline.entidades.Transaccion;
 @Repository
 public interface TransaccionRepository extends JpaRepository<Transaccion, Integer> {
     List<Transaccion> findByVendedor_Id(int id);
+
+    List<Transaccion> findByComprador_Id(int id);
 }
 
