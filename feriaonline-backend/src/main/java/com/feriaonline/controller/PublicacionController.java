10cd859a92f0f89d0affcaf370cc1fbdf5138716
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-23 16:36:19 -0300

    Update PublicacionController.java

--- feriaonline-backend/src/main/java/com/feriaonline/controller/PublicacionController.java
+++ feriaonline-backend/src/main/java/com/feriaonline/controller/PublicacionController.java
@@ -4,24 +4,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.feriaonline.dto.PublicacionRequestDTO;
+import com.feriaonline.entidadesDTO.ImagenPublicacionDTO;
 import com.feriaonline.entidadesDTO.PublicacionDTO;
 import com.feriaonline.service.PublicacionService;
-
 
 @RestController
 @RequestMapping("/publicaciones")
 @CrossOrigin(origins = "*") // permitir requests del frontend
 public class PublicacionController {
-    
+
     @Autowired
     PublicacionService publicacionService;
 
@@ -31,7 +34,14 @@ public class PublicacionController {
     }
 
     @PostMapping(value = "/crear", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public PublicacionDTO crearPublicacion(@RequestPart PublicacionRequestDTO dto, @RequestPart("imagenes") List<MultipartFile> imagenes) {
+    public PublicacionDTO crearPublicacion(@RequestPart PublicacionRequestDTO dto,
+            @RequestPart("imagenes") List<MultipartFile> imagenes) {
         return publicacionService.crearPublicacion(dto, imagenes);
+    }
+
+    @DeleteMapping("/imagen")
+    public ResponseEntity<String> eliminarImagen(@RequestBody ImagenPublicacionDTO dto) {
+        publicacionService.eliminarImagen(dto);
+        return ResponseEntity.ok("Imagen eliminada correctamente");
     }
 }
