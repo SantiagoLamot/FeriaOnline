064e5448fcb9960699db89398b1bdf2842f1f3c6
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-23 16:15:28 -0300

    Update PublicacionController.java

--- feriaonline-backend/src/main/java/com/feriaonline/controller/PublicacionController.java
+++ feriaonline-backend/src/main/java/com/feriaonline/controller/PublicacionController.java
@@ -3,11 +3,14 @@ package com.feriaonline.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.feriaonline.dto.PublicacionRequestDTO;
 import com.feriaonline.entidadesDTO.PublicacionDTO;
@@ -27,8 +30,8 @@ public class PublicacionController {
         return publicacionService.obtenerTodasLasPublicaciones();
     }
 
-    @PostMapping
-    public PublicacionDTO crearPublicacion(@RequestBody PublicacionRequestDTO dto) {
-        return publicacionService.crearPublicacion(dto);
+    @PostMapping(value = "/crear", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public PublicacionDTO crearPublicacion(@RequestPart PublicacionRequestDTO dto, @RequestPart("imagenes") List<MultipartFile> imagenes) {
+        return publicacionService.crearPublicacion(dto, imagenes);
     }
 }
