6a183254cb7ab554d13cf220178a0af60c0e28b1
Santiago Lamot <164097782+SantiagoLamot@users.noreply.github.com>
2025-06-24 15:13:20 -0300

    Update ImagenRepository.java

--- feriaonline-backend/src/main/java/com/feriaonline/repository/ImagenRepository.java
+++ feriaonline-backend/src/main/java/com/feriaonline/repository/ImagenRepository.java
@@ -1,9 +1,11 @@
 package com.feriaonline.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feriaonline.entidades.ImagenPublicacion;
 
 public interface ImagenRepository extends JpaRepository<ImagenPublicacion, Integer> {
-    
+    List<ImagenPublicacion> findByPublicacion_Id(int publicacionId);
 }
